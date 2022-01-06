228c9c0bb1603ba5195384a3b7a92320c789cfe8
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-06 11:05:15 -0300

    fix: resolved bug

--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,33 @@
-﻿namespace CSharp
+﻿using System.Collections;
+
+namespace CSharp
 {
     public class Principal
     {
         public static void Main(String[] args)
         {
-            var pessoa = new Pessoa();
-            pessoa.Nome = "Marcos";
-
+            int numero = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numero; i++)
+            {
+                int quant = 0;
+                int esferas = int.Parse(Console.ReadLine());
+                for (int j = 2; j <= esferas; j++)
+                {
+                    int countDiv = 0;
+                    for (int k = 1; k <= j; k++)
+                    {
+                        if (j % k == 0)
+                        {
+                            countDiv++;
+                        }
+                    }
+                    if (countDiv % 2 == 0)
+                    {
+                        quant++;
+                    }
+                }
+                Console.WriteLine(quant);
+            }
         }
     }
 }

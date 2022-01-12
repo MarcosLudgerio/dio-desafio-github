02e9d69f0af91a1d953459c05a31b8802aa156f6
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-12 11:08:04 -0300

    feat: implemented example matrix

--- linq-collections/Collections/Program.cs
+++ linq-collections/Collections/Program.cs
@@ -1,17 +1,35 @@
-﻿int[] arrayInt = new int[3];
+﻿// int[] arrayInt = new int[3];
 
-arrayInt[0] = 6;
-arrayInt[1] = 7;
-arrayInt[2] = 5;
+// arrayInt[0] = 6;
+// arrayInt[1] = 7;
+// arrayInt[2] = 5;
 
-System.Console.WriteLine("Varrendo array pelo for");
-for (int i = 0; i < arrayInt.Length; i++)
+// System.Console.WriteLine("Varrendo array pelo for");
+// for (int i = 0; i < arrayInt.Length; i++)
+// {
+//     System.Console.WriteLine(arrayInt[i]);
+// }
+
+// System.Console.WriteLine("Varrendo array com foreach");
+// foreach (var item in arrayInt)
+// {
+//     System.Console.WriteLine(item);
+// }
+
+int[,] matrix = new int[4, 4];
+Random random = new Random();
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    System.Console.WriteLine(arrayInt[i]);
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        matrix[i, j] = random.Next(0, 100);
+    }
 }
 
-System.Console.WriteLine("Varrendo array com foreach");
-foreach (var item in arrayInt)
+for (int i = 0; i < matrix.GetLength(0); i++)
 {
-    System.Console.WriteLine(item);
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        System.Console.WriteLine(matrix[i, j]);
+    }
 }

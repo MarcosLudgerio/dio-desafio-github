b092bdef3474222e7485b54f9ab7de9c3c976e37
MarcosLudgerio <raimundo.marcos@dcx.ufpb.br>
2022-01-12 10:22:36 -0300

    feat: implemented examples for to sweep arrays

--- linq-collections/Collections/Program.cs
+++ linq-collections/Collections/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] arrayInt = new int[3];
+
+arrayInt[0] = 6;
+arrayInt[1] = 7;
+arrayInt[2] = 5;
+
+System.Console.WriteLine("Varrendo array pelo for");
+for (int i = 0; i < arrayInt.Length; i++)
+{
+    System.Console.WriteLine(arrayInt[i]);
+}
+
+System.Console.WriteLine("Varrendo array com foreach");
+foreach (var item in arrayInt)
+{
+    System.Console.WriteLine(item);
+}
